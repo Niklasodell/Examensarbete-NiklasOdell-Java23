c2da13b43f7ba559c34726e425c6fccf6f16829e
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2025-05-18 22:11:23 +0200

    La in WebMvcConfigurer i CorsConfig. La också till korrekt adress. Dockerfile ändrad för att inte bygga maven varje gång.

--- src/main/java/com/examensarbete/application/CorsConfig.java
+++ src/main/java/com/examensarbete/application/CorsConfig.java
@@ -7,19 +7,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+    @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
             }
         };
-    }
-}
+
